8c004019aed35162bcae75304979cf3e30f1bf84
Sem 1 <sem1luckylongre@gmail.com>
2025-02-08 15:47:27 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    cors: {
+      origin: 'https://jtjfgy-5173.csb.app',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    },
+  },
 })
